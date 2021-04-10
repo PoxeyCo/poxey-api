30817f9a2be198d134576ca58b7f58562118d182
Gleb <maikezseller@gmail.com>
2021-04-10 21:26:58 +0300

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 const logger = require('./helpers/logger');
 const requestLogger = require('./middlewares/requestLogger');
@@ -10,15 +11,15 @@ const apiRoute = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://poxey-369b6.web.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://poxey-369b6.web.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    next();
-});
-
+//     next();
+// });
+app.use(cors())
 app.use(express.json());
 app.use(requestLogger);
 app.use(authProtect);
